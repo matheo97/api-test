71c0600e6918401d10f2e18fe6788f8480b72f95
Mateo Salazar <msalazar@velostics.com>
2024-03-08 08:41:42 -0500

    Add more details

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ app.post("/", (req, res) => {
   console.log("*********************************************");
   console.log("PAYLOAD");
   console.log(req.body);
+
+  // Printing query parameters
+  console.log("Query Params:");
+  console.log(req.query);
+
+  // Printing 'api-key' from headers
+  console.log("API Key:");
+  console.log(req.headers["api-key"]);
+
   res.send(req.body);
 });
 
